f47d2c1b622b117b2d89cdefff624343736f63cc
Jason <33882681+JasonHuang24@users.noreply.github.com>
2022-10-16 17:42:40 -0600

    Commit 12 -Improved Responsiveness -Improved navigation bar transition -Added highlighted sublink for nav bar -Fixed "refresh bandaid fix" for burger -Vertically centered corporate SJ logos -JQuery now implemented on head-sublinks

--- JS/index.js
+++ JS/index.js
@@ -17,7 +17,6 @@ const navSlide = () => {
     else {
       nav.classList.remove("nav-active");
       burger.classList.remove('on');
-      window.location.reload(); //fixes bug where window resizing if statement fires even after toggling the class to be removed.
     }
   });
 }
@@ -27,18 +26,26 @@ navSlide();
 let widthMatch = window.matchMedia("(min-width: 1080px)");
 let widthMatch2 = window.matchMedia("(max-width: 1080px)");
 // mm in the function arg is the matchMedia object, passed back into the function
+
 widthMatch.addEventListener('change', function(mm) {
   if (mm.matches) {
-      // it matches the media query: that is, min-width is >= 1080px
-        if (burgerDivEle.classList.contains('on') && navUlEle.classList.contains('nav-active')) { //bigger than 1080p clean up classes, so the nav bar is in the original state.
-        nav.classList.remove("nav-active");
-        burger.classList.remove('on');
-          widthMatch2.addEventListener('change', function(mm) { //shrink it back down to 1080p, re-add it, only under the condition that we removed it first.
-              if(mm.matches) {
-              nav.classList.add("nav-active");
-              burger.classList.add('on');
-            }
-          });
-        }
-      }
+    // it matches the media query: that is, min-width is >= 1080px
+    if (burgerDivEle.classList.contains('on') && navUlEle.classList.contains('nav-active')) { //bigger than 1080p clean up classes, so the nav bar is in the original state.
+      nav.classList.remove("nav-active");
+      burger.classList.remove('on');
+      nav.classList.add('been-removed')
+      burger.classList.add('been-removed')
+    }
+  }
+  });
+
+  widthMatch2.addEventListener('change', function(mm) { //shrink it back down to 1080p, re-add it, only under the condition that we removed it first.
+    if (mm.matches) {
+      if (burgerDivEle.classList.contains('been-removed') && navUlEle.classList.contains('been-removed')) {
+      nav.classList.add("nav-active");
+      burger.classList.add('on');
+      nav.classList.remove("been-removed");
+      burger.classList.remove('been-removed');
+    }
+    }
   });
